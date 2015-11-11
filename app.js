43f8030d167d2b8da3f55985a1229cb445e743de
Benjamin Margis <BenMargis@Benjamins-MacBook-Air.local>
2015-11-11 15:03:33 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var value = 42;
+
+if (value >= 53){
+	value = value + 42;
+}else{
+	value = value - 13;
+};
+
+var newString = "11";
+value = (value + newString);
+
+var newArray = [];
+
+for (var i = 0; i < value.length; i++) {
+	newArray = value.charAt[i];
+};
+
+newArray.shift();
+newArray.pop();
+
+var backwards = 0;
+
+for (var i = 2; i >= 0; i--) {
+	backwards = (backwards + [i]);
+};
+
+value = parseInt(value);
+backwards = parseInt(backwards);
